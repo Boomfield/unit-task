c76a2df3ed312b4e7fd9a691d4917cbdf23b6112
Boomfield <boomfield12@gmail.com>
2023-12-17 16:44:05 +0100

    added unit test for pen class

---
+++ src/test/java/utils/RandomGenerator.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.util.Random;
+
+public class RandomGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static String generateRandomString(int length) {
+        StringBuilder randomString = new StringBuilder(length);
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            randomString.append(randomChar);
+        }
+
+        return randomString.toString();
+    }
+
+    public static int generateRandomInt(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
+    }
+
+}
